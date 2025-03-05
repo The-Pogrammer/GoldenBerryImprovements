880399679d265404e4c4f98a2e609984fe2d096a
Pogrammer/Katelyn <gamepropikachu@gmail.com>
2025-03-05 03:38:57 -0500

    Remove clutter switch freezing

--- Source/GoldenBerryImprovementsModule.cs
+++ Source/GoldenBerryImprovementsModule.cs
@@ -32,12 +32,20 @@ public class GoldenBerryImprovementsModule : EverestModule {
 #endif
     }
 
+    private static ILHook lockBlock_UnlockRoutine, key_UseRoutine, absorbRoutineHook;
+
     public override void Load()
     {
-        Everest.Events.LevelLoader.OnLoadingThread += AddController;
+        absorbRoutineHook = new ILHook(
+            typeof(ClutterSwitch).GetMethod("AbsorbRoutine", BindingFlags.NonPublic | BindingFlags.Instance).GetStateMachineTarget(),
+            modAbsorbRoutine
+        );
 
-        //door speedup done by Viv!! (vividescence)
+        Everest.Events.LevelLoader.OnLoadingThread += AddController;
+        IL.Celeste.ClutterSwitch.OnDashed += modClutterSwitch;
         
+        //door speedup done by Viv!! (vividescence)
+
         MethodInfo m1 = typeof(LockBlock).GetMethod("UnlockRoutine", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance).GetStateMachineTarget();
         lockBlock_UnlockRoutine = new ILHook(m1, (il) => LockBlock_UnlockRoutine(il, m1.DeclaringType.GetField("<>4__this")));
         MethodInfo m2 = typeof(Key).GetMethod("UseRoutine", System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.Instance).GetStateMachineTarget();
@@ -46,7 +54,11 @@ public class GoldenBerryImprovementsModule : EverestModule {
 
     public override void Unload()
     {
+        IL.Celeste.ClutterSwitch.OnDashed -= modClutterSwitch;
+
         Everest.Events.LevelLoader.OnLoadingThread -= AddController;
+        absorbRoutineHook?.Dispose();
+        
         lockBlock_UnlockRoutine?.Dispose();
         key_UseRoutine?.Dispose();
     }
@@ -56,7 +68,47 @@ public class GoldenBerryImprovementsModule : EverestModule {
         level.Add(new Controller());
     }
 
-    private static ILHook lockBlock_UnlockRoutine, key_UseRoutine;
+    private void modAbsorbRoutine(ILContext il)
+    {
+        ILCursor cursor = new ILCursor(il);
+
+        if (cursor.TryGotoNext(MoveType.After, instr => instr.MatchLdcI4(11)))
+        {
+            cursor.Emit(OpCodes.Ldarg_0);
+            cursor.EmitDelegate(ShouldChangeAbsorbValue);
+        }
+    }
+
+    private static int ShouldChangeAbsorbValue(int prev, object stateMachine)
+    {
+        Type stateMachineType = stateMachine?.GetType();
+        FieldInfo playerField = stateMachineType?.GetField("player", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+        Player player = playerField?.GetValue(stateMachine) as Player;
+
+        if (player != null)
+        {
+            foreach (Follower f in player.Leader.Followers)
+            {
+                if (f.Entity is Strawberry s && s.Golden && !s.Winged)
+                {
+                    return 0;
+                }
+            }
+        }
+        return prev;
+    }
+
+    private void modClutterSwitch(ILContext il)
+    {
+        ILCursor cursor = new ILCursor(il);
+
+        if (cursor.TryGotoNext(MoveType.After, instr => instr.MatchLdcR4(0.2f)))
+        {
+            cursor.Emit(OpCodes.Pop);
+            cursor.Emit(OpCodes.Ldc_R4, 0f);
+        }
+    }
+    
     private static void LockBlock_UnlockRoutine(ILContext ctx, FieldInfo _this)
     {
         ILCursor cursor = new(ctx);
